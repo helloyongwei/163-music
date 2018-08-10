bc208bf7f09ed14897f0b82fb965e125be868416
helloyong <2628578405@qq.com>
2018-08-10 09:51:25 +0800

    点击歌曲时激活歌曲项

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,11 +9,18 @@
             let $el = $(this.el)
             $el.html(this.template)
             let {songs} = data
-            let liList = songs.map((song)=>$('<li></li>').text(song.name))
+            let liList = songs.map((song)=>
+                $('<li></li>').text(song.name).attr('data-song-id', song.id)
+            )
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
+        },
+        activeItem(li) {
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
         },
         clearActive() {
             $(this.el).find('.active').removeClass('active')
@@ -31,22 +38,37 @@
                 })
                 return songs
             })
-        }
+        },
     }
     let controller = {
         init(view, model) {
             this.view = view
             this.model = model
             this.view.render(this.model.data)
-            window.eventHub.on('upload', ()=>{
-                this.view.clearActive()
-            })
+            this.getAllSongs()
+            this.bindEventHub()
+            this.bindEvents()
             window.eventHub.on('create', (songData)=>{
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
-            this.model.find().then(()=>{
+            
+        },
+        getAllSongs() {
+            return this.model.find().then(()=>{
                 this.view.render(this.model.data)
+            })
+        },
+        bindEvents() {
+            $(this.view.el).on('click', 'li', (e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select', {id: songId})
+            })
+        },
+        bindEventHub(){
+            window.eventHub.on('upload', ()=>{
+                this.view.clearActive()
             })
         }
     }
