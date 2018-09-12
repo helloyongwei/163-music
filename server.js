49e81a38ee92172745330a4e34bb2adef57a76f2
helloyong <2628578405@qq.com>
2018-09-12 15:11:57 +0800

    README.md

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
-var port = process.argv[2]
 var qiniu = require('qiniu')
+var port = 8888
 
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
-}
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
@@ -18,10 +14,8 @@ var server = http.createServer(function(request, response){
   var queryObject = parsedUrl.query
   var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
-
   console.log('HTTP 路径为\n' + path)
-  if(path == '/uptoken'){
+ if(path == '/uptoken'){
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
     response.setHeader('Access-Control-Allow-Origin', '*')
     response.removeHeader('Date')
@@ -48,8 +42,8 @@ var server = http.createServer(function(request, response){
     response.end()
   }
 
-  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请打开http://localhost:' + port)
+console.log('successed')
+
